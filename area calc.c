e1fea88a53b27c716704888c6dedb9db5c6afb8f
Njuguna-arch <j44gichanga@gmail.com>
2024-10-13 21:05:25 +0300

    Add files via upload

---
+++ area calc.c
@@ -0,0 +1,16 @@
+//program to print area and perimeter of a rectangle
+/*author Joseph
+date: 13\10\24*/
+
+#include <stdio.h>
+int main()
+{
+float length = 49;
+float width = 38;
+
+printf("The area of the rectangle is : %.1f\n", length*width);
+printf("The perimeter of the rectangle is: %.1f", 2*(length + width));
+
+
+return 0;
+}
