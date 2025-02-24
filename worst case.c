96b8e1cbe868ebfdc409ce1e469db8bf7f053c28
Njuguna-Wanja01 <j44gichanga@gmail.com>
2025-02-24 16:59:13 +0300

    Add files via upload

---
+++ worst case.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int linear_search(int arr[], int size, int target)
+{
+    for (int i = 0;i < size; i++)
+    {
+        if(arr[i] == target)
+        {
+            return i; // target found
+        }
+    }
+    return -1; // target not found
+}
+int main()
+{
+    int arr[] = {2, 3, 4, 10, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 35;// target is not in the array
+
+    int result = linear_search(arr, size, target);
+    if (result != -1)
+    {
+        printf("Element found at index %d\n", result);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
+    return 0;
+}
